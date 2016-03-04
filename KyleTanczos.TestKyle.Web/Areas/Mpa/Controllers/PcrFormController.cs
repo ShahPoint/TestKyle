f1d3fc2e20bc1125dade0e71243587cbc30610b8
jay-d-shah <jay.shah@shahpoint.com>
2016-03-04 11:13:53 -0500

    Add different control Views

--- KyleTanczos.TestKyle.Web/Areas/Mpa/Controllers/PcrFormController.cs
+++ KyleTanczos.TestKyle.Web/Areas/Mpa/Controllers/PcrFormController.cs
@@ -8,34 +8,138 @@ using System.Web.Mvc;
 
 namespace KyleTanczos.TestKyle.Web.Areas.Mpa.Controllers
 {
-    public class Page
+    public class PcrForm
     {
-        public Tab Tabs { get; set; }
+        public PcrForm()
+        {
+            Tabs = new List<Tab>();
+        }
+
+        public List<Tab> Tabs { get; set; }
     }
 
     public class Tab
     {
-        public Section Sections { get; set; }
+        public Tab()
+        {
+            Sections = new List<Section>();
+        }
+
+        public List<Section> Sections { get; set; }
     }
 
     public class Section
     {
-        public Control Controls { get; set; }
+        public Section()
+        {
+            Controls = new List<Ctrl>();
+            NgWidth = 6;
+        }
+        public string SectionName { get; set; }
+        public int NgWidth { get; set; }
+        public List<Ctrl> Controls { get; set; }
     }
 
-    public class Control
+    public class Ctrl
+    {
+        public Ctrl()
+        {
+            NgWidth = 6;
+            DropDownOptions = new List<Select2Option>();
+        }
+        public string DisplayName { get; set; }
+        public ControlTypeEnum ControlType { get; set; }
+        public List<Select2Option> DropDownOptions { get; set; }
+        public int NgWidth { get; set; }
+        public string PlaceHolder { get; set; }
+        public string ClientId { get; set; }
+
+
+
+    }
+
+    public static class CtrlHelpers
     {
 
+        public static List<Select2Option> GetSelect2Options(IEnumerable<string> strOptions)
+        {
+            var returnOptions = strOptions.Select(x => new Select2Option() { id = x, text = x }).ToList();
+
+            return returnOptions;
+        }
+    }
+
+    public class Select2Option
+    {
+        public string id { get; set; }
+        public string text { get; set; }
     }
 
 
+    public enum ControlTypeEnum { TextBox, DropDownList, Select2, Select2Single, Select2Many, Select2TagsSingle, Select2TagsMany }
+
+   // public enum NgWidthEnum {ng1, ng2, ng3, ng4, ng5, ng6, ng7, ng8, ng9, ng10, ng11, ng12 }
     public class PcrFormController : Controller
     {
         // GET: Mpa/PcrForm
         public ActionResult Index()
         {
-            int john = 7;
-            return View(john);
+            PcrForm pcrForm = new PcrForm();
+
+            Tab tab = new Tab();
+
+            Section section = new Section() { SectionName = "Test 1", NgWidth = 9 };
+
+            Ctrl control = new Ctrl() { DisplayName = "FirstName", ControlType = ControlTypeEnum.Select2,
+                DropDownOptions = CtrlHelpers.GetSelect2Options(new string[] { "one", "two", "three" })
+            };
+
+
+
+            section.Controls.Add(control);
+
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+
+            tab.Sections.Add(section);
+
+
+            tab.Sections.Add(section);
+            tab.Sections.Add(section);
+
+            pcrForm.Tabs.Add(tab);
+
+            tab = new Tab();
+
+            section = new Section() { SectionName = "Raggedy Ann" }; ;
+
+            control = new Ctrl() { DisplayName = "DropDown", ControlType = ControlTypeEnum.DropDownList,
+            DropDownOptions = CtrlHelpers.GetSelect2Options(new string[] { "one", "two", "three" })
+            };
+
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+            section.Controls.Add(control);
+
+            tab.Sections.Add(section);
+            tab.Sections.Add(section);
+            tab.Sections.Add(section);
+
+            pcrForm.Tabs.Add(tab);
+
+
+
+            return View(pcrForm);
         }
 
         // GET: Mpa/PcrForm/Details/5
