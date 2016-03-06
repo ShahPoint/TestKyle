18905d7e282aed5f8d582d81ad5665fe63840427
jay-d-shah <jay.shah@shahpoint.com>
2016-03-06 12:34:38 -0500

    Testing heavier resources on phone

---
+++ KyleTanczos.TestKyle.Web/Areas/Mpa/Controllers/AppController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KyleTanczos.TestKyle.Web.Areas.Mpa.Controllers
+{
+    public class AppController : Controller
+    {
+        // GET: Mpa/App/
+        public ActionResult Install()
+        {
+            return View();
+        }
+    }
+}
