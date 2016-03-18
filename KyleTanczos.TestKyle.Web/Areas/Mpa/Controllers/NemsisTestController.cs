a7aefbdb148591b528bcfb5d33056a0dca34af30
jay-d-shah <jay.shah@shahpoint.com>
2016-03-18 14:45:50 -0400

    Add InputSpinner

---
+++ KyleTanczos.TestKyle.Web/Areas/Mpa/Controllers/NemsisTestController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KyleTanczos.TestKyle.Web.Areas.Mpa.Controllers
+{
+    public class NemsisModel
+    {
+        public string NemsisXml { get; set; }
+    }
+
+    public class NemsisTestController : Controller
+    {
+        // GET: Mpa/NemsisTest
+        public ActionResult Index()
+        {
+        NemsisModel nemsisXML = GetNemsisXml();
+            return View( nemsisXML);
+        }
+
+        private NemsisModel GetNemsisXml()
+        {
+            NemsisModel returnString = new NemsisModel();
+            returnString.NemsisXml = "yah i am here";
+
+            return returnString;
+        }
+    }
+}
