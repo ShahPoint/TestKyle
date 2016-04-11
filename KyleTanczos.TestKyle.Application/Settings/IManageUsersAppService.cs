c3d60ced4d3bdc9dc8c6fadb325c814c938f8773
kyle.tanczos <kyle.tanczos@shahpoint.com>
2016-04-11 03:37:14 -0400

    forgotten file

---
+++ KyleTanczos.TestKyle.Application/Settings/IManageUsersAppService.cs
@@ -0,0 +1,21 @@
+﻿using Abp.Application.Services;
+using Abp.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyleTanczos.TestKyle.Settings
+{
+
+    [DontWrapResult]
+    public interface IManageUsersAppService : IApplicationService
+    {
+        Task<List<UsersDto>> Get();
+
+        //Task<List<StationsDto>> CreateOrUpdateStations(List<StationsDto> stationsDto);
+        Task<UsersDto> Update(UsersDto usersDto);
+
+    }
+}
