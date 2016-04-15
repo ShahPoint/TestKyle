301f085dc83b2b190e4916b3dc3ae979fedbddb4
kyle.tanczos <kyle.tanczos@shahpoint.com>
2016-04-15 11:12:43 -0400

    Test for output cache

--- KyleTanczos.TestKyle.Web/Areas/Mpa/Controllers/DashboardController.cs
+++ KyleTanczos.TestKyle.Web/Areas/Mpa/Controllers/DashboardController.cs
@@ -8,6 +8,8 @@ namespace KyleTanczos.TestKyle.Web.Areas.Mpa.Controllers
     [AbpMvcAuthorize(AppPermissions.Pages_Tenant_Dashboard)]
     public class DashboardController : TestKyleControllerBase
     {
+
+        [OutputCache(Duration = 300, VaryByParam = "None")]
         public ActionResult Index()
         {
             return View();
